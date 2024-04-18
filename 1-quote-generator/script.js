73c6c4b79a4d078dc2d69f9f2b54f991b8d0db7d
Chloe <lnc3596@gmail.com>
2024-04-18 23:06:36 +0800

    [feat]名言產生器init

---
+++ 1-quote-generator/script.js
@@ -0,0 +1,64 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes=[];
+
+//Show Loading
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+//Hide Loading
+function complete(){
+    loader.hidden=true;
+    quoteContainer.hidden=false;
+}
+
+//Show New Quote
+function newQuote(){
+    loading()
+    // Pick a random quote from apiQuotes array
+    const newQuote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)]
+    // Check if Author field is blank and replace if with 'Unknown'
+    if(!newQuote.author){
+        newQuote.author = 'Unknown'
+    }else{
+        authorText.textContent = newQuote.author;
+    }
+    // Check Quote length to determine styling
+    if(newQuote.text.length>120){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = newQuote.text;
+    complete()
+}
+
+function tweetQuote(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click',tweetQuote)
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }catch (error){
+    }
+}
+
+//On Load
+getQuotes();
